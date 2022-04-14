ed096366f4b7a6dced59e5b63b378da1d05dd86d
Lincol1999 <lincol_mr_25@hotmail.com>
2022-04-14 18:40:28 -0500

    Creación de un microservicio Item con Feign terminado

--- src/main/java/com/groupitem/springbootservicioitem/SpringbootServicioItemApplication.java
+++ src/main/java/com/groupitem/springbootservicioitem/SpringbootServicioItemApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-//VIDEO 13: USO DEL OPENFEIGN PARA CONSUMO DE NUESTRA API
+
 @EnableFeignClients //habilita nuestros clientes Feign que tengamos implementa en nuestro proyecto, inyecta en nuestros controladores.
 @SpringBootApplication
 public class SpringbootServicioItemApplication {
